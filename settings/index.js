43d6128c510eaba7cb0200fb9b35b5faae6802a4
Tin <tinmarkoviccs@gmail.com>
2017-03-21 17:27:59 +0100

    Edited settings load to accept variable paths

--- settings/index.js
+++ settings/index.js
@@ -2,12 +2,16 @@ const merge = require('lodash/merge');
 
 const defaultConfig = require('./default.json');
 
+var pathToLocalConfig = process.env.TXOC_LOGGER_SETTINGS ? 
+                        process.env.TXOC_LOGGER_SETTINGS : 
+                        './txoc-logger.settings.json';
+
 try {
-  const localConfig = require('../mqtt-node-logger.settings.json');
+  const localConfig = require(pathToLocalConfig);
   config = merge(defaultConfig, localConfig);
 } catch (e) {
   if (e.code !== 'MODULE_NOT_FOUND') {
-    console.log(`Module "local.js" failed to load. Error: ${e.message}`);
+    console.log(`Settings failed to load. Error: ${e.message}`);
   } else {
     console.log(`Settings file not found, loading default settings.`);
     config = defaultConfig;
